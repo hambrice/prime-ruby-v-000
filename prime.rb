a35e59e5bb35675aec93c8aa8caea2f248b73858
Brad Hambrice <hambrice@uab.edu>
2018-01-19 05:15:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 def prime?(integer)
   if integer <= 1
     return false
-    end 
-    array = (2..integer).to_a
-    array.pop
-    if array.any? do |number|
-    integer % number == 0
-  end
+  end 
+  array = (2..integer).to_a
+  array.pop
+  if array.any? do |number|
+        integer % number == 0
+    end
     return false
   else 
     return true 
